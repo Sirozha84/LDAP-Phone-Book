be8dd67707b1ad66d5b8b0fa8d0102c0c4aff0c7
Сергей Гордеев <sirozhagordeev@gmail.com>
2019-12-10 10:13:32 +0700

    Прототип
    Прототип... пока читает данные из файла, выгружаемого powershell-скриптом.
    В целом уже работает, но надо доработать поиск.

---
+++ LDAP Phone Book/FormMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LDAP_Phone_Book
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.listViewBook = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderNum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(668, 20);
+            this.textBoxSearch.TabIndex = 0;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.TextBoxSearch_TextChanged);
+            // 
+            // listViewBook
+            // 
+            this.listViewBook.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewBook.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderNum,
+            this.columnHeaderEmail});
+            this.listViewBook.HideSelection = false;
+            this.listViewBook.Location = new System.Drawing.Point(12, 38);
+            this.listViewBook.Name = "listViewBook";
+            this.listViewBook.Size = new System.Drawing.Size(668, 366);
+            this.listViewBook.TabIndex = 1;
+            this.listViewBook.UseCompatibleStateImageBehavior = false;
+            this.listViewBook.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "ФИО";
+            this.columnHeaderName.Width = 200;
+            // 
+            // columnHeaderNum
+            // 
+            this.columnHeaderNum.Text = "Телефон";
+            this.columnHeaderNum.Width = 100;
+            // 
+            // columnHeaderEmail
+            // 
+            this.columnHeaderEmail.Text = "Email";
+            this.columnHeaderEmail.Width = 200;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 416);
+            this.Controls.Add(this.listViewBook);
+            this.Controls.Add(this.textBoxSearch);
+            this.Name = "FormMain";
+            this.Text = "LDAP Phone Book";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.ListView listViewBook;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderNum;
+        private System.Windows.Forms.ColumnHeader columnHeaderEmail;
+    }
+}
+
---
+++ LDAP Phone Book/FormMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LDAP_Phone_Book
+{
+    public partial class FormMain : Form
+    {
+        List<Contact> book;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            string[] strings = System.IO.File.ReadAllText("c:\\Users\\SG\\Desktop\\ist_users.csv").Split('\r');
+            book = new List<Contact>();
+            for (int i = 2; i < strings.Count(); i++)
+            {
+                strings[i] = strings[i].Replace("\",\"", ",");
+                strings[i] = strings[i].Replace("\n", "");
+                strings[i] = strings[i].Replace("\"", "");
+                string[] s = strings[i].Split(',');
+                if (s.Count() > 5)
+                    book.Add(new Contact(s[3], s[4], s[0]));
+            }
+            Redraw();
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        void Redraw()
+        {
+            listViewBook.BeginUpdate();
+            listViewBook.Items.Clear();
+            foreach (Contact cn in book)
+            {
+                //if (cn.name.ToLower().Contains(textBoxSearch.Text.ToLower()))
+                if (cn.name.ToLower().Substring(0, textBoxSearch.Text.Length) == textBoxSearch.Text.ToLower())
+                {
+                    ListViewItem item = new ListViewItem(cn.name);
+                    item.SubItems.Add(cn.phone);
+                    item.SubItems.Add(cn.email);
+                    listViewBook.Items.Add(item);
+                }
+            }
+            listViewBook.EndUpdate();
+        }
+    }
+}
